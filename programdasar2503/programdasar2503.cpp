1e8f6a97439d47b5d1d42a0ffba0844dcb9d4245
Ekoramadhan13 <enugraha411@gmail.com>
2024-03-25 10:16:06 +0700

    kedua

--- programdasar2503/programdasar2503.cpp
+++ programdasar2503/programdasar2503.cpp
@@ -8,7 +8,13 @@ void input()
 	cout << "Bilangan pertama = ";
 	cin >> a;
 	 
-	
-   
+	cout << "Bilangan kedua = ";
+	cin >> b;
+}
+
+int penjumlahan(int bil_1, int bil_2) 
+{
+	return bil_1 + bil_2;
+
 }
 
